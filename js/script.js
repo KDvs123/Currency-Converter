ac92f8f59d369b9fd5c2df5adeb350e6008018f8
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-06-13 12:30:03 +0530

    Creating Options Tag & Inserting it inside Select Tag

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const dropList=document.querySelectorAll(".drop-list select");
+
+for(let i=0;i<dropList.length;i++){
+    for(currency_code in country_code){
+        //selecting USD by default as FROM currency and LKR as TO currency
+        let selected;
+        if(i==0){
+            selected=currency_code=="USD" ? "selected" : "";
+        }else if(i==1){
+            selected = currency_code == "LKR" ? "selected" : "";
+        }
+        //creating option tag with passing currency code as a text and value
+        let optionTag = `<option value="${currency_code}"${selected}>${currency_code}</option>`;
+        //inserting options tag inside select tag
+        dropList[i].insertAdjacentHTML("beforeend",optionTag);
+    }
+}
