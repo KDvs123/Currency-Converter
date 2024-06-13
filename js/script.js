13edeab922fb9a53f94d10ae38d7268f0660eaf4
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-06-13 15:41:13 +0530

    Creating Function, Sending Request to API, Retrieving Data From API

--- js/script.js
+++ js/script.js
@@ -1,23 +1,50 @@
-const dropList=document.querySelectorAll(".drop-list select"),
-getButton=document.querySelector("form button");
+const dropList = document.querySelectorAll(".drop-list select"),
+  fromCurrency = document.querySelector(".from select"),
+  toCurrency = document.querySelector(".to select"),
+  getButton = document.querySelector("form button");
 
-for(let i=0;i<dropList.length;i++){
-    for(currency_code in country_code){
-        //selecting USD by default as FROM currency and LKR as TO currency
-        let selected;
-        if(i==0){
-            selected=currency_code=="USD" ? "selected" : "";
-        }else if(i==1){
-            selected = currency_code == "LKR" ? "selected" : "";
-        }
-        //creating option tag with passing currency code as a text and value
-        let optionTag = `<option value="${currency_code}"${selected}>${currency_code}</option>`;
-        //inserting options tag inside select tag
-        dropList[i].insertAdjacentHTML("beforeend",optionTag);
+
+for (let i = 0; i < dropList.length; i++) {
+  for (currency_code in country_code) {
+    //selecting USD by default as FROM currency and LKR as TO currency
+    let selected;
+    if (i == 0) {
+      selected = currency_code == "USD" ? "selected" : "";
+    } else if (i == 1) {
+      selected = currency_code == "LKR" ? "selected" : "";
     }
+    //creating option tag with passing currency code as a text and value
+    let optionTag = `<option value="${currency_code}"${selected}>${currency_code}</option>`;
+    //inserting options tag inside select tag
+    dropList[i].insertAdjacentHTML("beforeend", optionTag);
+  }
 }
 
-getButton.addEventListener("click", e=>{
-    e.preventDefault();//preventing form from submitting
-    getExchangeRate();
-})
+getButton.addEventListener("click", (e) => {
+  e.preventDefault(); //preventing form from submitting
+  getExchangeRate();
+});
+
+
+
+
+function getExchangeRate() {
+  const amount = document.querySelector(".amount input");
+  let amountVal = amount.value;
+  //if user dont enter any value or enter 0 then we'll put 1 value by default in the input field
+  if (amountVal == "" || amountVal == "0") {
+    amount.value = "1";
+    amountVal = 1;
+  }
+  let url = `https://v6.exchangerate-api.com/v6/6737e3a7d4e128184c3ea1d7/latest/${fromCurrency.value}`;
+  //fetching api response and returning it with parsing into js obj and in another then method necevinf that object
+
+  fetch(url).then(response=> response.json().then(result =>{
+    let exchangeRate=result.conversion_rates[toCurrency.value];
+    let totalExchangeRate=(amountVal*exchangeRate).toFixed(2);
+    const exchangeRateTxt=document.querySelector(".exchange-rate");
+    exchangeRateTxt.innerText=`${amountVal} ${fromCurrency.value} = ${totalExchangeRate} ${toCurrency.value}`
+  }));
+}
+
+
